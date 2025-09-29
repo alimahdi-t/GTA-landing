71d94902a093060aca2008eabe72a9f19f6a964e
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-09-29 12:58:13 +0330

    Install some libs and add tailwind css styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-const App = () => {
-    return(
-        <div>App </div>
-    )
-}
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all.js";
 
-export default App
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl">Welcome to World of GTA and Gsap!</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
