05b1d4dd3e594162622495ee264690c58cc470e3
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-10-01 10:00:13 +0330

    Implement Hero section with animation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all.js";
+import { ScrollTrigger } from "gsap/all";
 import Hero from "./sections/Hero.jsx";
 import Navbar from "./sections/Navbar.jsx";
 
